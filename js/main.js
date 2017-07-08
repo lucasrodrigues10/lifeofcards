688ec345b107c0908a53035141a8a26925f20a46
Lucas <lucasrodrigues10@outlook.com>
2017-07-08 12:52:45 -0300

    Removi a pagina jogar e juntei com o inicio

--- js/main.js
+++ js/main.js
@@ -6,38 +6,27 @@ $(document).ready(function() {
     $('[data-toggle="tooltip"]').tooltip();
 
     $('#myModal').modal('show');
-    $('.btn-jogar').click(function() {
-        $('.jogar').show();
-        $('.home').hide();
-        $('.perfil').hide();
-        $('.inventario').hide();
-        $('.loja').hide();
-    });
     $('.btn-perfil').click(function() {
         $('.perfil').show();
         $('.home').hide();
-        $('.jogar').hide();
         $('.inventario').hide();
         $('.loja').hide();
     });
     $('.btn-inventario').click(function() {
         $('.inventario').show();
         $('.home').hide();
-        $('.jogar').hide();
         $('.perfil').hide();
         $('.loja').hide();
     });
     $('.btn-loja').click(function() {
         $('.loja').show();
         $('.home').hide();
-        $('.jogar').hide();
         $('.perfil').hide();
         $('.inventario').hide();
     });
     $('.btn-home').click(function() {
         $('.home').show();
         $('.loja').hide();
-        $('.jogar').hide();
         $('.perfil').hide();
         $('.inventario').hide();
     });
