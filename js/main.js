2a16e7a9b928f56cbb09cfee29d752611fa191b0
Lucas <lucasrodrigues10@outlook.com>
2017-07-10 10:37:18 -0300

    Mudei para grafico 3d e mudei posicao

--- js/main.js
+++ js/main.js
@@ -30,6 +30,54 @@ $(document).ready(function() {
         $('.perfil').hide();
         $('.inventario').hide();
     });
+    //Gráfico 3d
+    google.charts.load("current", { packages: ["corechart"] });
+    google.charts.setOnLoadCallback(drawChart);
 
 
+
+    function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+            ['Task', 'Hours per Day'],
+            ['Eagle', 11],
+            ['Viking', 2],
+            ['Demon', 2],
+            ['Angel', 2],
+            ['Sword', 7]
+        ]);
+
+        var options = {
+            backgroundColor: "transparent",
+            title: 'Melhores Decks',
+            titleTextStyle: {
+                color: 'white', // any HTML string color ('red', '#cc00cc')
+                fontName: 'Times New Roman', // i.e. 'Times New Roman'
+                fontSize: 14, // 12, 18 whatever you want (don't specify px)
+                bold: true, // true or false
+                italic: false // true of false
+            },
+            legend: {
+                textStyle: {
+                    color: 'white'
+                }
+            },
+            is3D: true,
+            width: 600,
+            height: 500,
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('piechart_3d'));
+        chart.draw(data, options);
+    }
+
+    function resizeChart() {
+        chart.draw(data, options);
+    }
+    if (document.addEventListener) {
+        window.addEventListener('resize', resizeChart);
+    } else if (document.attachEvent) {
+        window.attachEvent('onresize', resizeChart);
+    } else {
+        window.resize = resizeChart;
+    }
 });
