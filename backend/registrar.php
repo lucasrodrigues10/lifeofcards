b62e2482fcf5873821d1ac03d54ae6e52b3bab00
lucas <lucasrodrigues10@outlook.com>
2017-07-13 23:09:31 -0300

    testando winscp

---
+++ backend/registrar.php
@@ -0,0 +1,11 @@
+<?php
+	$email = $_POST['email'];
+	$usuario = $_POST['usuario'];
+	$senha = $_POST['senha'];
+
+	echo "Email: ".$email."";
+	echo "Usuario: ".$usuario."";
+	echo "Senha: ".$senha."";
+
+
+?>
