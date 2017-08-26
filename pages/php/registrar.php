d7592c712327cef52094d48dff9f13502464b161
lucas <lucasrodrigues10@outlook.com>
2017-08-26 20:17:25 -0300

    Envia email para informar registro com sucesso

--- pages/php/registrar.php
+++ pages/php/registrar.php
@@ -9,7 +9,7 @@
   $situacaoE = ""; //erro de email
   $situacaoU = ""; //erro de usuario igual
   $situacao = ""; //erros gerais
-
+  $situacao = ""; //erro email
   
   /* Procura se já tem um usuário idêntico */
   $query = "SELECT * FROM Usuario WHERE Login = '$usuario'";
@@ -30,13 +30,26 @@
   if(($situacaoE === "") && ($situacaoU === "")){//insere se nao tiver iguais
     $query = "INSERT INTO Usuario (Login,Senha,Email) VALUES ('$usuario','$senha','$email');";
     if($conn->query($query)){
+    	/* Email para informar o registro*/
+		$subject = 'Life of Cards - Registrado com sucesso';
+		$message = 
+		'Bem vindo, '. $usuario .'!' . "\r\n" .
+		'O seu registro foi realizado com sucesso. ' . "\r\n";
+		$headers = 'From: lifeofcards@lifeofcards.site11.com' . "\r\n" .
+	    'Reply-To: webgame10@outlook.com' . "\r\n" .
+	    'X-Mailer: PHP/' . phpversion();
+		$envio = mail($email, $subject, $message, $headers);
+		if($envio)
+			$situacaoM = "sucesso";
+		else
+			$situacaoM = "erro";
       	$query = "SELECT * FROM Usuario WHERE Login = '$usuario' AND Senha = '$senha'";
 		$result = $conn->query($query);		
 		$row = $result->fetch_assoc();
 		$_SESSION["id"] = $row["IDusuario"];
       $situacao = "Registrado com sucesso! ID: ". $row["IDusuario"] ." ";
       ?>
-        <script> location.replace("../main.php"); </script>
+        <script> //location.replace("../main.php"); </script>
       <?php
     } else {
       $situacao = "Não foi registrado";
@@ -56,9 +69,10 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
   </head>
   <body>
-    <h1>1:<?=$situacao?></h1>
-    <h1>2:<?=$situacaoU?></h1>
-    <h1>3:<?=$situacaoE?></h1>
+    <h1>Geral:<?=$situacao?></h1>
+    <h1>Usuario igual:<?=$situacaoU?></h1>
+    <h1>Email igual:<?=$situacaoE?></h1>
+    <h1>Envio de email:<?=$situacaoM?></h1>
 
     <!-- jQuery first, then Tether, then Bootstrap JS. -->
     <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
