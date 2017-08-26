29c7a6b5b7a7b29f1cadc8d71c1a8683a2f34e48
lucas <lucasrodrigues10@outlook.com>
2017-08-26 05:49:13 -0300

    Adicionei o arquivo htaccess para mostrar os erros de php

--- pages/php/db.php
+++ pages/php/db.php
@@ -1,16 +1,31 @@
 <?php
+
 /*
+
   $usuario = "root";
+
   $senha = "123";
+
 */
 
+
+
   $host = 'localhost'; 
-  $user = 'id2237061_admin';
-  $pass = 'aparecido123';
-  $db = 'id2237061_lifeofcards';
+
+  $usuario = 'id2237061_admin';
+
+  $senha = 'aparecido123';
+
+  $database = 'id2237061_lifeofcards';
+
   $conn = new mysqli($host,$usuario,$senha,$database);
 
+
+
   if($conn->connect_error){
+
     die("Erro: " . $conn->connect_error);
+
   }
+
 ?>
