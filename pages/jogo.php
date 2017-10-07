7a25190161605d3bfc41806a95d1d4611f462091
lucas <lucasrodrigues10@outlook.com>
2017-10-06 23:53:22 -0300

    canvas dentro de div e espaco para cartas e botoes

--- pages/jogo.php
+++ pages/jogo.php
@@ -1,42 +1,26 @@
 <!DOCTYPE html>
-
-
 <html>
-
-
 <head>
-
-
     <title>Jogo</title>
-
-
     <link rel="shortcut icon" type="image/x-icon" href="../img/icon/favicon.ico"/>
 
-
     <!--Ter todos os simbolos -->
-
-
     <meta charset="utf-8">
 
 
     <!--Zoom em tablets/mobile-->
-
-
     <meta name="mobile-web-app-capable" content="yes">
-
-
     <meta name="viewport"
           content="width=device-width, height=device-height, initial-scale=1, maximum-scale=1, shrink-to-fit=no">
 
 
     <!--Boostrap 4.0 CDN -->
-
-
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css"
           integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
-
-
+    <!--Site CSS -->
     <link rel="stylesheet" type="text/css" href="../css/style.css">
+    <!-- Jogo CSS -->
+    <link rel="stylesheet" type="text/css" href="../css/jogo.css">
 
 
     <!-- Phaser -->
@@ -44,27 +28,67 @@
     <script src="../js/functions.js"></script>
     <script type="text/javascript" src="jogo.js"></script>
 
+    <!--Icones do google -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
 
     <style>
-        html, body, .ct, .ct > div {
+        html, body, #ct, #ct > div {
             height: 100%;
         }
 
-        .ct > div {
+        #ct > div {
             float: left;
+        }
+
+        .carta {
+
+        }
+
+        #ct-cartas {
+            overflow: scroll;
+            height: 40%;
+        }
+        .carta > img{
+            width: 33%;
+        }
+        #left > button, #right {
+            display: block;
+            margin: auto;
+            width: 40%;
+            height: auto;
         }
     </style>
 </head>
 
 
 <body>
-<div class="ct">
-    <div style="background-color: red;width: 20%;">
-
+<div id="ct">
+    <div id="left" style="background-color: red;width: 10%;">
+        <button class="btn btn-warning" href="javascript:void(0)" onclick="toggleFullScreen()">
+            <i class="material-icons" id="btn-fullscreen">fullscreen</i>
+        </button>
     </div>
-    <div style="background-color: blue;width: 60%" id="phaser-game"></div>
-    <div style="background-color: green;width: 20%">
+    <div style="background-color: blue;width: 60%" id="center"></div>
+    <div style="background-color: green;width: 30%" id="right">
+        <div id="ct-cartas">
+            <div class="carta">
+                <img src="https://pbs.twimg.com/profile_images/1233627159/TibiaIcon_400x400.png">
+            </div>
+            <div class="carta">
+                <img src="https://pbs.twimg.com/profile_images/1233627159/TibiaIcon_400x400.png">
+            </div>
+            <div class="carta">
+                <img src="https://pbs.twimg.com/profile_images/1233627159/TibiaIcon_400x400.png">
+            </div>
+            <div class="carta">
+                <img src="https://pbs.twimg.com/profile_images/1233627159/TibiaIcon_400x400.png">
+            </div>
+            <div class="carta">
+                <img src="https://pbs.twimg.com/profile_images/1233627159/TibiaIcon_400x400.png">
+            </div>
 
+        </div>
     </div>
 </div>
 
@@ -90,9 +114,9 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js"
         integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn"
         crossorigin="anonymous"></script>
-
-
 </body>
+<!-- Fullscreen script -->
+<script type="text/javascript" src="../js/fullscreen.js"></script>
 
 
 </html>
